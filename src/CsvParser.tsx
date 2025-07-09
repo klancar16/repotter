37b538c50896f113057b0af7ce5a9f15083fe09b
Jaka Klančar <klancar16@gmail.com>
2025-07-09 12:03:05 +0200

    Redesign the whole app and simply some parts of finding chains

--- src/CsvParser.tsx
+++ src/CsvParser.tsx
@@ -1,11 +1,13 @@
-import React, { type ChangeEvent } from "react";
+import React, { type ChangeEvent, useState } from "react";
 import Papa from "papaparse";
 import { useData } from "./context/data.ts";
 import type { PotType } from "./types";
 import { useStatus } from "./context/status.ts";
 import { tableHeaders } from "./utils.ts";
+import { CloseIcon, FileIcon } from "./assets/Icons.tsx";
 
 const CsvParser: React.FC = () => {
+    const [fileName, setFileName] = useState<string | null>(null);
     const { setData, setIsLoading } = useData();
     const { setStatus } = useStatus();
 
@@ -35,6 +37,7 @@ const CsvParser: React.FC = () => {
 
                 setData(results.data);
                 setStatus("Successfully read CSV");
+                setFileName(fileContent.name);
             },
             error: (error) => {
                 setStatus(`Error reading CSV ${error.message}`);
@@ -42,19 +45,48 @@ const CsvParser: React.FC = () => {
         });
     };
 
+    const handleClear = () => {
+        setData([]);
+        setFileName(null);
+        setStatus("Successfully cleared input.");
+    };
+
     return (
-        <div className="flex flex-row gap-3 me-4 py-4">
-            <label htmlFor="csv-upload" className="text-sm font-medium text-gray-700">
-                Upload CSV File:
-            </label>
-            <input
-                id="csv-upload"
-                type="file"
-                name="file-input"
-                accept=".csv"
-                onChange={parseCsvFile}
-                className="file:bg-gray-50 border-0"
-            />
+        <div className="bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md">
+            <h2 className="text-xl font-bold mb-1 text-gray-900 dark:text-gray-100">Get Started</h2>
+            <p className="text-gray-500 dark:text-gray-400 mb-4">Upload your CSV file to begin planning.</p>
+            {!fileName ? (
+                <div className="file-upload-box">
+                    <div className="space-y-1 text-center">
+                        <div className="flex text-sm text-gray-600 dark:text-gray-300 justify-center">
+                            <label htmlFor="file-upload" className="file-upload">
+                                <span>Upload a file</span>
+                                <input
+                                    id="file-upload"
+                                    name="file-upload"
+                                    type="file"
+                                    className="sr-only"
+                                    onChange={parseCsvFile}
+                                    accept=".csv"
+                                />
+                            </label>
+                        </div>
+                        <p className="text-xs text-gray-500 dark:text-gray-400">CSV up to 10MB</p>
+                    </div>
+                </div>
+            ) : (
+                <div className="mt-4">
+                    <div className="flex items-center justify-between bg-green-50 dark:bg-gray-700 p-3 rounded-lg">
+                        <div className="flex items-center space-x-3">
+                            <FileIcon />
+                            <span className="text-sm font-medium text-gray-700 dark:text-gray-200">{fileName}</span>
+                        </div>
+                        <button onClick={handleClear} className="clear-input-file">
+                            <CloseIcon />
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
--- src/Table.tsx
+++ src/Table.tsx
@@ -10,14 +10,18 @@ const Table: React.FC = () => {
         if (typeof value === "boolean") {
             if (value) {
                 return (
-                    <td className="tag bg-green-500 text-white">
-                        <span aria-label="Yes">&#10003;</span>
+                    <td className="tag">
+                        <span aria-label="Yes" className="yes">
+                            Yes
+                        </span>
                     </td>
                 );
             } else {
                 return (
-                    <td className="tag bg-red-500 text-white">
-                        <span aria-label="No">&#9747;</span>
+                    <td className="tag">
+                        <span aria-label="No" className="no">
+                            No
+                        </span>
                     </td>
                 );
             }
@@ -33,33 +37,38 @@ const Table: React.FC = () => {
                     <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
                 </div>
             ) : (
-                <table aria-label="Data">
-                    <caption className="sr-only">Pot information and attributes</caption>
-                    <thead>
-                        <tr>
-                            {tableHeaders.map((header) => {
-                                return (
-                                    <th scope="col" key={header}>
-                                        {header}
-                                    </th>
-                                );
-                            })}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data
-                            .filter((value) => value)
-                            .map((value) => {
-                                return (
-                                    <tr className="border odd:bg-white even:bg-gray-100" key={value.id}>
-                                        {tableHeaders.map((header) =>
-                                            handleTableRowCell(value[header as keyof PotType]),
-                                        )}
-                                    </tr>
-                                );
-                            })}
-                    </tbody>
-                </table>
+                <div className="bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md">
+                    <h2 className="text-xl font-bold mb-4 text-gray-900 dark:text-gray-100">Data Source</h2>
+                    <div className="overflow-x-auto">
+                        <table aria-label="Data">
+                            <caption className="sr-only">Pot information and attributes</caption>
+                            <thead className="bg-gray-50 dark:bg-gray-700">
+                                <tr>
+                                    {tableHeaders.map((header) => {
+                                        return (
+                                            <th scope="col" key={header}>
+                                                {header}
+                                            </th>
+                                        );
+                                    })}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data
+                                    .filter((value) => value)
+                                    .map((value) => {
+                                        return (
+                                            <tr className="border odd:bg-white even:bg-gray-100" key={value.id}>
+                                                {tableHeaders.map((header) =>
+                                                    handleTableRowCell(value[header as keyof PotType]),
+                                                )}
+                                            </tr>
+                                        );
+                                    })}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             )}
         </>
     );
